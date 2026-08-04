6448c61c7f7e32b0d2d1fddbc6ffaeefd936638f
SilverKamui <ammartourabi07@proton.me>
2026-08-04 18:38:40 +0100

    some kitty settings

--- modules/apps/kitty/kitty.nix
+++ modules/apps/kitty/kitty.nix
@@ -15,6 +15,14 @@
     config = {
       args = lib.mkAfter (lib.optionals (config.shell != "") [config.shell]);
       settings = {
+            enable_audio_bell = "no";
+
+            font_size = 14;
+            font_family = "Fira Code Regular Nerd Font Complete Mono";
+            bold_font = "auto";
+            italic_font = "auto";
+            
+            background_opacity = "0.95";
         
         ];
       };
