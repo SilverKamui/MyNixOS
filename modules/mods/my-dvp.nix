349b998f73289f815afb18d2efbd0dc7f89d7e33
Kamui <ammartourabi07@proton.me>
2026-08-04 17:17:42 +0100

    full dvp

--- modules/mods/my-dvp.nix
+++ modules/mods/my-dvp.nix
@@ -1,4 +1,32 @@
 {self, inputs, ...}: {
-  flake.nixosModules.myDvp = { config, lib, pkgs, modulesPath, ... }: {
-          };
+  flake.nixosModules.myDvp = { config, lib, pkgs, modulesPath, ... }: let
+      buildLayout = {
+        name,
+        description,
+        lang,
+        symbols,
+      }: let
+        xkbcomp = lib.getExe pkgs.xorg.xkbcomp;
+        compilationOutputFile = pkgs.runCommand "${name}-compiled-keyboard-layout" {} ''
+          (${xkbcomp} ${symbols} 2> $out) || true
+        '';
+        compilationOutput = builtins.readFile compilationOutputFile;
+        compilationSuccess = compilationOutput == "";
+        errorMessage = "Failure compiling layout [${name}]: ${compilationOutput}";
+        ifCompilationSuccess = lib.throwIfNot compilationSuccess errorMessage;
+      in {
+        ${name} = ifCompilationSuccess {
+          description = description;
+          languages = [lang];
+          symbolsFile = symbols;
+        };
+      };
+    in {
+      services.xserver.xkb.extraLayouts = buildLayout {
+        name = "my-dvp";
+        description = "programmer dvorak with ordered numbers";
+        lang = "eng";
+        symbols = ./my-dvp.xkb;
+      };
+    }
 }
