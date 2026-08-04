db9b470670f79f41f70bcac4e41861d299a19206
SilverKamui <ammartourabi07@proton.me>
2026-08-04 16:06:20 +0100

    fixed noctalia

--- modules/apps/noctalia/noctalia.nix
+++ modules/apps/noctalia/noctalia.nix
@@ -1,6 +1,7 @@
 { self, inputs, ...}: { 
     perSystem = { pkgs, ...}: {
         packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+            inherit pkgs;
             settings = 
                 (builtins.fromJSON
                     (builtins.readFile ./noctalia.json)).settings;
