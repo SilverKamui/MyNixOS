8d55b416db84ba8dfaaf44709a58f17f41a2b1c5
Kamui <ammartourabi07@proton.me>
2026-08-04 15:50:27 +0100

    noctalia

---
+++ modules/apps/noctalia/noctalia.nix
@@ -0,0 +1,7 @@
+{ self, inputs, ...}: { 
+    perSystem = { pkgs, ...}: {
+        packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+            settings = {};
+        };
+    };
+}
