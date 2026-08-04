506b829864bb3edb2e453fe2600c5ee51ce086aa
SilverKamui <ammartourabi07@proton.me>
2026-08-04 16:03:26 +0100

    noctalia

--- modules/apps/noctalia/noctalia.nix
+++ modules/apps/noctalia/noctalia.nix
@@ -1,7 +1,9 @@
 { self, inputs, ...}: { 
     perSystem = { pkgs, ...}: {
         packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-            settings = {};
+            settings = 
+                (builtins.fromJSON
+                    (builtins.readFile ./noctalia.json)).settings;
         };
     };
 }
