827e281fed5ca1cb7a7d6fd2e88b070b47b96e26
SilverKamui <ammartourabi07@proton.me>
2026-08-20 20:07:51 +0100

    modularised it

--- modules/hosts/KamuiGaming/configuration.nix
+++ modules/hosts/KamuiGaming/configuration.nix
@@ -102,25 +102,7 @@
       ];
 
 
-      # Nvidia
-      services.xserver.videoDrivers = ["nvidia"];
-
-      hardware.nvidia = {
-        modesetting.enable = true;
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
-        open = true;
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
-      };
-
-      hardware.nvidia.prime = {
-        offload.enable = true;
-
-        nvidiaBusId = "PCI:1@0:0:0";
-        amdgpuBusId = "PCI:101@0:0:0";
-      };
-
+      # Nvidia 
 #      boot.kernelPackages = pkgs.linuxPackages_latest;
 #      boot.kernelParams = [
 #          "nvidia-drm.modeset=1"
--- modules/mods/nvidia.nix
+++ modules/mods/nvidia.nix
@@ -1,5 +1,22 @@
 { inputs, ... }: {
     flake.nixosModules.nvidia = { config, inputs, ... }: {
-        hardware.ddcci.enable  = true;
+        services.xserver.videoDrivers = ["nvidia"];
+
+        hardware.nvidia = {
+            modesetting.enable = true;
+            powerManagement.enable = false;
+            powerManagement.finegrained = false;
+            open = true;
+            nvidiaSettings = true;
+            package = config.boot.kernelPackages.nvidiaPackages.stable;
+        };
+
+        hardware.nvidia.prime = {
+            offload.enable = true;
+
+            nvidiaBusId = "PCI:1@0:0:0";
+            amdgpuBusId = "PCI:101@0:0:0";
+        };
+
     };
 }
