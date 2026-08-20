f4e9052c2810fc4971a793970ff0552830b47f4d
SilverKamui <ammartourabi07@proton.me>
2026-08-20 20:08:07 +0100

    and enabled

--- modules/hosts/KamuiGaming/default.nix
+++ modules/hosts/KamuiGaming/default.nix
@@ -3,7 +3,7 @@
   flake.nixosConfigurations.KamuiGaming = inputs.nixpkgs.lib.nixosSystem {
     modules = [
       self.nixosModules.KamuiGamingConfiguration
-      #self.nixosModules.nvidia
+      self.nixosModules.nvidia
 
       ### Keyboard
       self.nixosModules.myDvp
