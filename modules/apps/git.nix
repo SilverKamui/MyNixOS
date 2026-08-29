77c8fabd37bfaa8b91ca978403d8b69558a4e165
SilverKamui <ammartourabi07@proton.me>
2026-08-29 20:39:47 +0100

    no openssh

--- modules/apps/git.nix
+++ modules/apps/git.nix
@@ -6,16 +6,5 @@
         ];
         programs.ssh.startAgent = true;
 
-        services.openssh = {
-            enable = true;
-            settings = {
-                PasswordAuthentication = false;
-                KbdInteractiveAuthentication = false;
-                PermitRootLogin = "no";
-                AllowUsers = [ "kamui" ];
-                MaxAuthTries = 3;
-                PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
-            };
-        };
     };
 }
